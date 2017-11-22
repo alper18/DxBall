ded39753d133aaeeba5e4ce6e0eca59dab8d371b
riadahmedzakir <33862995+riadahmedzakir@users.noreply.github.com>
2017-11-22 14:55:21 +0600

    Initial commit

---
+++ app/src/main/java/com/example/riad/dxball/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.riad.dxball;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+        getSupportActionBar().hide();
+        setContentView(new GameCanvas(this));
+    }
+}
